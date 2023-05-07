cdac4b2013b4cbe773f55dc238e2e62ae9bdd1d2
shivam2512 <shivamshinde786@gmail.com>
2023-05-07 17:40:20 +0530

    GETELEMTBYCLASSNAME ADDED

--- dom.js
+++ dom.js
@@ -14,3 +14,14 @@ var addItem=document.getElementById('addItems');
 console.log(addItem);
 addItem.style.fontWeight = "bold";
 addItem.style.color = "green";
+
+//GETELEMTBYCLASSNAME: THIRD ELEMENT IN GREEN BACKGROUND AND ALL LIST ELEMENTS IN BOLD
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[2].style.backgroundColor="green";
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight = "bold";
+}
+
+
