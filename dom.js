83957b9cce4bf306377ec7b7afe92451e3701fbf
shivam2512 <shivamshinde786@gmail.com>
2023-05-07 17:20:53 +0530

    Commit Dom

---
+++ dom.js
@@ -0,0 +1,16 @@
+//TITLE WITH BLACK BORDER
+
+console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+console.log(headerTitle);
+header.style.borderBottom='solid 3px #000';
+
+
+//ADD ITEMS BOLD WITH GREEN COLOR
+
+console.log(document.getElementById('addItems'));
+var addItem=document.getElementById('addItems');
+console.log(addItem);
+addItem.style.fontWeight = "bold";
+addItem.style.color = "green";
